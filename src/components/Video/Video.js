e96eb2794c6f6dc903bd24b8ea5873f8ee107696
Luis Melendez <lmele014@fiu.edu>
2021-10-27 21:50:48 -0400

    added video description

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,9 +1,9 @@
-import "../Video/Video.scss"
-import data from "../../data/videos.json"
+import "./Video.scss"
+import data from "../../data/video-details.json"
 
 function Video() {
   return (
-    <div className="container">
+    <div className="video-container">
       <video className="video" controls poster={data[0].image}></video>
     </div>
   );
