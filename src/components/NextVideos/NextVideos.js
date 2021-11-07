098539471fbed66f482509a1bf46a14c6a27986d
Luis Melendez <lmele014@fiu.edu>
2021-11-07 18:36:00 -0500

    final sprint-2 commit

--- src/components/NextVideos/NextVideos.js
+++ src/components/NextVideos/NextVideos.js
@@ -7,16 +7,6 @@ export default function NextVideos(props) {
      return (
         <div className="videos-container">
             <p className="header-text">NEXT VIDEOS</p>
-<<<<<<< HEAD
-            {this.props.data.filter((video) => (video !== this.props.currentVideo))
-            .map((video, index) => (
-                <VideoCard
-                key={index}
-                clickHandler={this.props.clickHandler}
-                id={this.props.data.indexOf(video)}
-                video={video}/>
-            ))}
-=======
             {props.videos.map((video) => {
                 return (
                     <Link key={video.id} to={`/video/${video.id}`} className="link">
@@ -24,7 +14,6 @@ export default function NextVideos(props) {
                     </Link>
                 )
             })}
->>>>>>> sprint-2
         </div>
     );
 }
